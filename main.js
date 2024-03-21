035241ac62c5ceafcc36d59a5245e4d45a684b72
Skell87 <brandon.heisler@gmail.com>
2024-03-20 22:50:37 -0400

    fixed array

--- main.js
+++ main.js
@@ -14,9 +14,9 @@ function randomValueFromArray(array){
 const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
 /* these arrays contain the random elements of the story */
-const insertx = [Willy the Goblin, Big Daddy, Father Christmas];
-const inserty = [the soup kitchen, Disneyland, the White House];
-const insertz = [spontaneously combusted, melted into a puddle on the sidewalk, turned into a slug and crawled away];
+const insertx = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const inserty = ["the soup kitchen", "Disneyland", "the White House"];
+const insertz = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
 
 
